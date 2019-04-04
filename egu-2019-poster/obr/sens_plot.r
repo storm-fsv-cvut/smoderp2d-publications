800b43c857a10c1baba24b9d970820a5464a09d5
jerabekjak <jerabekjaku@gmail.com>
2019-04-04 18:21:05 +0200

    captions of figures and cometics

--- egu-2019-poster/obr/sens_plot.r
+++ egu-2019-poster/obr/sens_plot.r
@@ -1,16 +1,16 @@
-setwd("~/ownCloud/Knihovna/poster/EGU2019_smod/obr")
+setwd("~/ownCloud/Knihovna/poster/EGU2019_smod/egu-2019-poster/obr/")
 load_ = TRUE; if (load_) {load('sens.rda')}
 
 
 width_ = 2300*2.5
-height_ = 1200*2.5
+height_ = 1200*2.
 res_  = 600
 pointsize_ = 6*2.5
 png_ = TRUE
 
 if (png_) {png('sens.png',width = width_,height = height_,res = res_, pointsize = pointsize_)}
 par(mar = c(3,3,1,1))
-boxplot(sens, log = 'y')
+boxplot(sens, log = 'y',lwd=2)
 mtext('log10(sum of squeares [m/s]^2)', line = 2, side = 2)
 mtext('scenario with incresed or deceased parameter', line = 2, side = 1)
 abline(h=0)
